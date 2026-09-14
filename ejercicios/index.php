2466eb6a55adb1aeafe82b36cfa2e1c419df63a4
KevinJosueCruzVillalta <144403416+KevinJosueCruzVillalta@users.noreply.github.com>
2026-09-13 18:57:33 -0600

    agregando mas cosas

--- ejercicios/index.php
+++ ejercicios/index.php
@@ -8,7 +8,29 @@ comentario*/
 
 //Variables
 
-$my_string = "Esto es una cadena en string";
+$my_string = "Esto es una cadena de texto";
 $my_string = "Aqui cambio el valor de la cadena de texto";
 echo $my_string . "\n";
+echo gettype($my_string) . "\n";
+
+$my_string = 6; //Tipado dinamico
+echo $my_string . "\n";
+echo gettype($my_string) . "\n";
+
+$my_string = "Esto es una cadena de texto";
+
+$my_int = 7;
+$my_int = $my_int + 3;
+echo $my_int . "\n";
+echo $my_int - 1 . "\n";
+echo $my_int . "\n";
+echo gettype($my_int) . "\n";
+
+$my_double = 6.5;
+echo gettype($my_double) . "\n";
+echo $my_int + $my_double. "\n";
+
+$my_bool = true;
+echo $my_bool . "\n";
+echo gettype($my_bool) . "\n";
 ?>
