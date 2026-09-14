4e74a037f462918ede562692bdca1e4766dec535
KevinJosueCruzVillalta <144403416+KevinJosueCruzVillalta@users.noreply.github.com>
2026-09-13 18:37:07 -0600

    Aprendiendo fundamentos de php

--- ejercicios/index.php
+++ ejercicios/index.php
@@ -1,3 +1,14 @@
 <?php
-echo "hola mundo";
+// hola mundo
+echo "hola mundo\n";
+
+/*comentario
+comentario*/
+
+
+//Variables
+
+$my_string = "Esto es una cadena en string";
+$my_string = "Aqui cambio el valor de la cadena de texto";
+echo $my_string . "\n";
 ?>
