d8eea290387491ce96484b03fec7c89410292d4f
KevinJosueCruzVillalta <144403416+KevinJosueCruzVillalta@users.noreply.github.com>
2026-09-14 18:22:20 -0600

    Variables en PHP

---
+++ ejercicios/variables.php
@@ -0,0 +1,30 @@
+<?php
+  //Variables
+  /*
+  es un espacio en memoria reservado para algun dato
+  o informacion.
+  */
+
+  /**
+   * Reglas para declarar variables
+   * - no se inician con numeros
+   * - no se declaran variables repetidas
+   * - no llevan espacion ni caracteres especiales
+   * - no se pueden usar palabras reservadas
+   */
+
+  //PHP es de tipado debil, eso quiere decir que no lleva tipo de dato.
+
+  //Varibales numericas:
+  $miVariable = 5; //entero
+  //Variables con punto flotante o decimal
+  $miFlotante = 8.9; //decimal
+  //Variables de tipo cadena de texto
+  $miString = "Hola mundo"; //cadena de texto
+  $miString2 = 'Hola mundo'; 
+  //Variables tipo booleano
+  $miBoleando = true; //booleano
+  $miBoleando2 = false; 
+  $miBoleando3 = 1; //Verdadero
+  $miBoleando4 = 0; //Falso
+?>
