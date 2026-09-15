16df76bd4c47326bbcfbef7bb39f89eeaee47d57
KevinJosueCruzVillalta <144403416+KevinJosueCruzVillalta@users.noreply.github.com>
2026-09-15 17:10:01 -0600

    funciones sin parametros

---
+++ ejercicios/funcion_sin_parametro.php
@@ -0,0 +1,21 @@
+<?php
+
+//funciones
+//es un bloque de codigo reutilizable
+
+/*function saludar(){
+    echo "Hola";
+}
+
+saludar();
+saludar();*/
+
+//funciones sin parametros de retorno
+
+function mostrarCurso(){
+    echo "Este es el curso de php 8";
+}
+
+mostrarCurso();
+
+?>
