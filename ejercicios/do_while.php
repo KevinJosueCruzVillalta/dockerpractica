a55e812a49e416575e7aa2175ed793754c97be4a
KevinJosueCruzVillalta <144403416+KevinJosueCruzVillalta@users.noreply.github.com>
2026-09-15 15:40:08 -0600

    do_while

---
+++ ejercicios/do_while.php
@@ -0,0 +1,24 @@
+<?php
+
+//si necesitas al menos una iteracion segura
+//y despues una evaluacion usa do while
+
+//validacion simple
+
+do{
+    $respuesta = "no";
+    echo "Continuar? (si/no): $respuesta";
+}while($respuesta === "si");
+
+echo "<hr>";
+
+//iteracion garantizada
+
+$contador = 1;
+do{
+    echo "Ejecucion #$contador";
+    $contador = $contador + 1; //contador ++;
+    echo "<br>"
+}while($contador <= 3);
+
+?>
