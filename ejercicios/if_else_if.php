365d70019787a0c6b0248e17a6157ebc88cba877
KevinJosueCruzVillalta <144403416+KevinJosueCruzVillalta@users.noreply.github.com>
2026-09-15 12:40:19 -0600

    if_else_if

---
+++ ejercicios/if_else_if.php
@@ -0,0 +1,31 @@
+<?php
+
+//determinar el dia de la semana segun un numero
+$diaNumero = 3;
+
+if($diaNumero == 1){
+    echo "Es lunes";
+} 
+elseif($diaNumero == 2){
+    echo "Es martes";
+}
+elseif($diaNumero == 3){
+    echo "Es miercoles";
+}
+elseif($diaNumero == 4){
+    echo "Es jueves";
+}
+elseif($diaNumero == 5){
+    echo "Es viernes";
+}
+elseif($diaNumero == 6){
+    echo "Es sabado";
+}
+elseif($diaNumero == 7){
+    echo "Es domingo";
+}else{
+    echo "dia invalido";
+}
+
+
+?>
