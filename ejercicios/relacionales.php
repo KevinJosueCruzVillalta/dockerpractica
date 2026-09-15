60d45eab70dea868b77b08c21b25fda483625010
KevinJosueCruzVillalta <144403416+KevinJosueCruzVillalta@users.noreply.github.com>
2026-09-14 21:57:21 -0600

    Operadores relacionales

---
+++ ejercicios/relacionales.php
@@ -0,0 +1,21 @@
+<?php
+
+$x = 10;
+$y = "10";
+$z = 15;
+
+var_dump($x == $y);
+echo "<br>";
+var_dump($x === $y);
+echo "<br>";
+var_dump($x != $z);
+echo "<br>";
+var_dump($x !== $y);
+echo "<br>";
+var_dump($x < $z);
+echo "<br>";
+var_dump($x > $z);
+
+//Nave espacial
+echo 5 <=> 3;
+?>
