9792dd3f00e343cccbee85e255a3155babf8f1cf
KevinJosueCruzVillalta <144403416+KevinJosueCruzVillalta@users.noreply.github.com>
2026-09-15 16:51:20 -0600

    alternativa

---
+++ ejercicios/alternativa_ec.php
@@ -0,0 +1,21 @@
+<?php
+
+$edad = 18;
+if ($edad >= 18):
+    echo "mayor de edad";
+
+endif;
+
+/*while():
+
+endwhile():*/
+
+for($i = 0; $i <=10; $i++){
+    #Codigo
+}
+
+/*foreach():
+
+endforeach;*/
+
+?>
