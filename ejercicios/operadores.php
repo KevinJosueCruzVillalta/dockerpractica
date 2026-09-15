99b63fc7678fd69fe1771c1f2a44092b3435d396
KevinJosueCruzVillalta <144403416+KevinJosueCruzVillalta@users.noreply.github.com>
2026-09-14 18:40:58 -0600

    Operadores Aritmeticos

---
+++ ejercicios/operadores.php
@@ -0,0 +1,23 @@
+<?php
+
+$a = 10;
+$b = 3;
+
+echo $a + $b; //13
+echo "<hr>";
+echo $a - $b; //7
+echo "<hr>";
+echo $a * $b; //30
+echo "<hr>";
+echo $a / $b; //3.3333333333333
+echo "<hr>";
+echo $a % $b; //1
+echo "<hr>";
+echo $a ** $b; //1000
+
+//Concatenacion
+$Nombre = "Kevin";
+$saludo = "hola";
+echo $saludo . $Nombre; //hola Kevin
+
+?>
