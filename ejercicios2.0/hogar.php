055771fc5fdb99058fc498e7788ac04f5607300d
KevinJosueCruzVillalta <144403416+KevinJosueCruzVillalta@users.noreply.github.com>
2026-09-16 00:10:45 -0600

    post

---
+++ ejercicios2.0/hogar.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Procesar formularios con post</h1>
+    <form action="recibido.php" method="post">
+        <label for="">Escribe tu nombre</label>
+        <br>
+        <input type="text" name="nombre">
+        <br>
+        <label for="">Escribe tu materia</label>
+        <br>
+        <input type="text" name="materia">
+        <br>
+        <button>Enviar</button>
+    </form>
+</body>
+</html>
